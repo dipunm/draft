80e85ffcbcbfe2da30257f9856504736a79a33cd
dipunm <dipunm@gmail.com>
2016-07-29 03:15:40 +0100

    Added Tests and implemented CuratedPageController

---
+++ Shopomo.Web.Tests/CuratedPageControllerLoadsContentFromCmsDynamically.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using Shopomo.ContentProvider;
+using Shopomo.ContentProvider.Models;
+using Shopomo.Web.Controllers;
+using Shouldly;
+
+namespace Shopomo.Web.Tests
+{
+    [TestFixture]
+    public class CuratedPageControllerLoadsContentFromCmsDynamically
+    {
+        [Test]
+        public async Task Controller_GivenAnUnknownPageName_ShouldRespondNotFound()
+        {
+            var knownPages = new[] {"known1", "known2"};
+            var controller = new CuratedPageController(null, knownPages);
+
+            var result = await controller.Desktop("unknown");
+
+            result.ShouldBeOfType<HttpNotFoundResult>();
+        }
+
+        [Test]
+        public async Task Controller_GivenKnownPageName_ShouldPresentDataFromContentProvider()
+        {
+            var knownPages = new[] { "known1", "known2" };
+            var contentProvider = new Mock<IContentProvider>();
+            var mockModel = new Mock<IContent>().Object;
+            contentProvider.Setup(p => p.GetPageAsync("known1"))
+                .ReturnsAsync(mockModel);
+            var controller = new CuratedPageController(contentProvider.Object, knownPages);
+
+            var result = await controller.Desktop("known1");
+
+            result.ShouldBeOfType<ViewResult>();
+            (result as ViewResult).Model.ShouldBe(mockModel);
+        }
+
+        [Test]
+        public async Task Controller_WhenLoadingMobilePage_ShouldUseMobilePage()
+        {
+            var knownPages = new[] { "known1", "known2" };
+            var contentProvider = new Mock<IContentProvider>();
+            var mockModel = new Mock<IContent>().Object;
+            contentProvider.Setup(p => p.GetPageAsync("known1"))
+                .ReturnsAsync(mockModel);
+            var controller = new CuratedPageController(contentProvider.Object, knownPages);
+
+            var result = await controller.Mobile("known1");
+
+            result.ShouldBeOfType<ViewResult>();
+            (result as ViewResult).ViewName.ShouldBe("mobile");
+        }
+
+        [Test]
+        public async Task Controller_WhenContentProviderReturnsNoContent_ShouldRespondServerError()
+        {
+            var knownPages = new[] { "known1", "known2" };
+            var contentProvider = new Mock<IContentProvider>();
+            contentProvider.SetReturnsDefault(Task.FromResult<IContent>(null));
+            var controller = new CuratedPageController(contentProvider.Object, knownPages);
+
+            var result = await controller.Desktop("known1");
+
+            result.ShouldBeOfType<HttpStatusCodeResult>();
+            (result as HttpStatusCodeResult).StatusCode.ShouldBe((int)HttpStatusCode.InternalServerError);
+        }
+
+        //TODO:???
+        public void Controller_WhenContentProviderReturnsNoContent_ShouldLogError()
+        {
+
+        }
+
+
+    }
+}
---
+++ Shopomo.Web/Controllers/CuratedPageController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Shopomo.ContentProvider;
+using Shopomo.ContentProvider.Models;
+
+namespace Shopomo.Web.Controllers
+{
+    //TODO: Caching
+
+    [RoutePrefix("content")]
+    public class CuratedPageController : Controller
+    {
+        private readonly IContentProvider _contentProvider;
+        private readonly IEnumerable<string> _supportedPages;
+
+        public CuratedPageController(IContentProvider contentProvider, string[] knownPages)
+        {
+            _contentProvider = contentProvider;
+            _supportedPages = knownPages;
+        }
+
+        //TODO: implement DI and remove this controller
+        public CuratedPageController()
+        {
+            _contentProvider = new DummyProvider();
+            _supportedPages = new List<string>()
+            {
+                "pagea", "pageb", "AboutUs"
+            };
+        }
+
+        [Route("{pageName}mobile")]
+        public Task<ActionResult> Mobile(string pageName) => LoadPage(pageName, "mobile");
+
+        [Route("{pageName}")]
+        public Task<ActionResult> Desktop(string pageName) => LoadPage(pageName, "desktop");
+
+        private async Task<ActionResult> LoadPage(string pageName, string viewName)
+        {
+            if (!_supportedPages.Contains(pageName))
+                return HttpNotFound();
+
+            var page = await _contentProvider.GetPageAsync(pageName);
+            if (page == null)
+            {
+                //TODO: logging?
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Content provider was unable to provide content.");
+            }
+            return View(viewName, page);
+        }
+    }
+
+
+    //TODO: Remove this class
+    public class DummyProvider : IContentProvider
+    {
+        public Task<IContent> GetPageAsync(string pageName)
+        {
+            return Task.FromResult<IContent>(new DummyContent("pageName", pageName));
+        }
+
+        public class DummyContent : IContent
+        {
+            public DummyContent(string title, string content)
+            {
+                Title = title;
+                Content = content;
+            }
+
+            public string Title { get; }
+            public string Content { get; }
+        }
+    }
+}
